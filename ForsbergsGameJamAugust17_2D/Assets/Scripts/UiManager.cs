e174eb9948b79dedb90a0c36d129d4e017636e70
Tobias Nylin <tobnyl@gmail.com>
2017-08-19 20:25:35 +0200

    Save

---
+++ ForsbergsGameJamAugust17_2D/Assets/Scripts/UiManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UiManager : MonoBehaviour 
+{
+    #region Fields/Properties
+
+    public string SceneNameToLoad;
+
+    private static UiManager _instance;
+
+    public static int Keys { get; set; }
+
+    public static UiManager Instance
+    {
+        get { return _instance; }
+    }
+
+    #endregion
+    #region Events
+
+    void Awake()
+	{
+		
+	}
+	
+	void Start() 
+	{
+		
+	}
+
+	void Update() 
+	{
+		if (Input.GetButtonDown("Next"))
+        {
+
+        }
+	}
+	
+	#endregion
+	#region Methods
+	
+	
+	
+	#endregion
+}
