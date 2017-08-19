46020748effff5c06d173189f5484cc3cb18eb86
Tobias Nylin <tobnyl@gmail.com>
2017-08-19 17:29:38 +0200

    Scroll camera

---
+++ ForsbergsGameJamAugust17_2D/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour 
+{
+    #region Fields/Properties
+
+    public GameObject Player;
+	
+	#endregion
+	#region Events
+	
+	void Awake()
+	{
+		
+	}
+	
+	void Start() 
+	{
+		
+	}
+
+	void Update() 
+	{
+		if (Player.transform.position.y < transform.position.y)
+        {
+            var temp = transform.position;
+            temp.y = Player.transform.position.y;
+            transform.position = temp;
+        }
+	}
+	
+	#endregion
+	#region Methods
+	
+	
+	
+	#endregion
+}
