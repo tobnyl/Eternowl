15eb0e94cd46025591039e56f07fb10dfafcac6c
Tobias Nylin <tobnyl@gmail.com>
2017-08-19 15:15:02 +0200

    Horizontal movement

---
+++ ForsbergsGameJamAugust17_2D/Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    #region Fields/Properties
+
+    public float Force;
+
+    private Rigidbody _rigidBody;
+
+    #endregion
+    #region Events
+
+    void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {        
+        _rigidBody.AddForce(new Vector3(Input.GetAxis("Horizontal"), 0, 0) * Force );
+    }
+
+    #endregion
+    #region Methods
+
+
+
+    #endregion
+}
